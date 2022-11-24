720d6b0c9260bfde15e663ce9fda629cdb43c0a2
nguyenquochuy2311 <nguyenquochuyhbt@gmail.com>
2022-11-24 13:22:33 +0700

    done crud

--- src/controllers/api/v1/board.controller.js
+++ src/controllers/api/v1/board.controller.js
@@ -1,5 +1,6 @@
-const Board = require("../../../models").BoardModel;
+const createError = require("http-errors");
 
+const Board = require("../../../models").BoardModel;
 const validateCreateBoardForm = require("../../../validation/board/create");
 
 module.exports = {
@@ -20,8 +21,8 @@ module.exports = {
     // GET param /:id
     getById: async (req, res, next) => {
         try {
-            const id = req.params.id;
-            const board = await Board.findByPk(id ,{
+            const boardId = req.params.id;
+            const board = await Board.findByPk(boardId, {
                 attributes: ['id', 'title']
             });
             return res.json(board);
@@ -31,15 +32,59 @@ module.exports = {
     },
 
     // POST
-    create: async(req, res, next) => {
+    create: async (req, res, next) => {
         try {
             const board = req.body;
 
             await validateCreateBoardForm(board);
-            
+
             const boardSaved = await Board.create(board);
 
             return res.json(boardSaved);
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    // PUT param /:id
+    update: async (req, res, next) => {
+        try {
+            const boardId = req.params.id;
+
+            const board = await Board.findByPk(boardId);
+            if (!board) return next(createError.BadRequest("Board not found"));
+
+            const { title } = req.body;
+
+            const boardUpdated = await Board.update({
+                title: title || board.title
+            }, {
+                where: {
+                    id: boardId
+                }
+            });
+
+            return boardUpdated[0] ? res.json({ message: "Update success" }) : next(createError.BadRequest("Update failed"));
+        } catch (error) {
+            next(error);
+        }
+    },
+
+    // DELETE param /:id
+    destroy: async (req, res, next) => {
+        try {
+            const boardId = req.params.id;
+
+            const board = await Board.findByPk(boardId);
+            if (!board) return next(createError.BadRequest("Board not found"));
+
+            const boardDeleted = await Board.destroy({
+                where: {
+                    id: boardId
+                }
+            });
+            
+            return boardDeleted ? res.json({ message: "Delete success" }) : next(createError.BadRequest("Delete failed"));
         } catch (error) {
             next(error);
         }
--- src/routes/api/v1/board.router.js
+++ src/routes/api/v1/board.router.js
@@ -7,6 +7,10 @@ router.get("/", boardController.getAll);
 
 router.get("/:id", boardController.getById);
 
-router.post("/", boardController.create)
+router.post("/", boardController.create);
+
+router.put("/:id", boardController.update);
+
+router.delete("/:id", boardController.destroy);
 
 module.exports = router;

14629fb2320073d8ea7c208ea058aeaf9e7fa6d9
nguyenquochuy2311 <nguyenquochuyhbt@gmail.com>
2022-11-25 11:10:08 +0700

    remove field router

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,15 +1,12 @@
 /** Define api v1 router */
 const v1BoardRouter = require("./board.router");
 const v1BoardItemRouter = require("./board-item.router");
-const v1FieldRouter = require("./field.router");
 /** End define api v1 router */
 
 const v1ApiRouter = (app) => {
     app.use("/api/v1/board", v1BoardRouter);
 
     app.use("/api/v1/board-item", v1BoardItemRouter);
-
-    app.use("/api/v1/field", v1FieldRouter);
 };
 
 module.exports = v1ApiRouter;
